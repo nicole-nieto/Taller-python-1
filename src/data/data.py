6cea46cd01957c4a355f3ff5f5d032839ded93f7
Nicole <nicolnieto48@gmail.com>
2025-09-07 14:42:49 -0500

    espacios duplicados

--- src/data/data.py
+++ src/data/data.py
@@ -19,7 +19,7 @@ class Data:
         return -1
     
 
-    def eliminar_duplicados(lista):
+    def eliminar_duplicados(self, lista):
         resultado = []
         vistos = []
         for elem in lista:
